b92468ff0f7d9be6b68722202c229038fbcdccbe
Yash Tripathi <tripathiyash97@gmail.com>
2018-05-14 02:09:40 +0530

    Numb

---
+++ numb.rb
@@ -0,0 +1,41 @@
+# Numb intro
+use_bpm 65
+
+# Part 1
+play :A4
+sleep 0.3
+play :C5
+sleep 0.3
+play :A4
+sleep 0.3
+play :D5, sustain: 0.5
+sleep 1
+play :F5, sustain: 0.5
+sleep 1
+play :E5
+
+sleep 1.3
+
+# Part 2
+play :A4
+sleep 0.3
+play :C5
+sleep 0.3
+play :A4
+sleep 0.3
+play :F5, sustain: 0.5
+sleep 1
+play :E5, sustain: 0.5
+sleep 1
+play :C5
+
+sleep 1.3
+
+# Part 3
+play :A4
+sleep 0.3
+play :C5
+sleep 0.3
+play :A4
+sleep 0.3
+play :D5, sustain: 0.7
